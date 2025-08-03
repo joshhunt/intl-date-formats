ec38ea43e4ce2586a27f2a9801ef11d32b9f3f87
joshhunt <git@joshhunt.dev>
2025-08-03 11:14:18 +0100

    use official action

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
+  cacheDir: "node_modules/.vite",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
